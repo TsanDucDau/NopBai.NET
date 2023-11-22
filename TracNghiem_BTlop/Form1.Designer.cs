d8ec6091d8b8aca6a4ac167ebc887fbed9c5903e
TsanDucDau <dauductran1234@gmail.com>
2023-11-22 16:15:01 +0700

    baitaptrenlop(TracNghiem)

---
+++ TracNghiem_BTlop/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TracNghiem_BTlop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lb_1 = new System.Windows.Forms.Label();
+            this.rbtn_A = new System.Windows.Forms.RadioButton();
+            this.rbtn_B = new System.Windows.Forms.RadioButton();
+            this.rbtn_C = new System.Windows.Forms.RadioButton();
+            this.rbtn_D = new System.Windows.Forms.RadioButton();
+            this.btn_next = new System.Windows.Forms.Button();
+            this.btn_back = new System.Windows.Forms.Button();
+            this.btn_send = new System.Windows.Forms.Button();
+            this.lb_2 = new System.Windows.Forms.Label();
+            this.lb_3 = new System.Windows.Forms.Label();
+            this.lb_4 = new System.Windows.Forms.Label();
+            this.lb_5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lb_1
+            // 
+            this.lb_1.AutoSize = true;
+            this.lb_1.Location = new System.Drawing.Point(25, 9);
+            this.lb_1.Name = "lb_1";
+            this.lb_1.Size = new System.Drawing.Size(41, 13);
+            this.lb_1.TabIndex = 0;
+            this.lb_1.Text = "Câu 1 :";
+            // 
+            // rbtn_A
+            // 
+            this.rbtn_A.AutoSize = true;
+            this.rbtn_A.Location = new System.Drawing.Point(28, 80);
+            this.rbtn_A.Name = "rbtn_A";
+            this.rbtn_A.Size = new System.Drawing.Size(32, 17);
+            this.rbtn_A.TabIndex = 1;
+            this.rbtn_A.TabStop = true;
+            this.rbtn_A.Text = "A";
+            this.rbtn_A.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_B
+            // 
+            this.rbtn_B.AutoSize = true;
+            this.rbtn_B.Location = new System.Drawing.Point(28, 103);
+            this.rbtn_B.Name = "rbtn_B";
+            this.rbtn_B.Size = new System.Drawing.Size(32, 17);
+            this.rbtn_B.TabIndex = 2;
+            this.rbtn_B.TabStop = true;
+            this.rbtn_B.Text = "B";
+            this.rbtn_B.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_C
+            // 
+            this.rbtn_C.AutoSize = true;
+            this.rbtn_C.Location = new System.Drawing.Point(28, 126);
+            this.rbtn_C.Name = "rbtn_C";
+            this.rbtn_C.Size = new System.Drawing.Size(32, 17);
+            this.rbtn_C.TabIndex = 3;
+            this.rbtn_C.TabStop = true;
+            this.rbtn_C.Text = "C";
+            this.rbtn_C.UseVisualStyleBackColor = true;
+            // 
+            // rbtn_D
+            // 
+            this.rbtn_D.AutoSize = true;
+            this.rbtn_D.Location = new System.Drawing.Point(28, 149);
+            this.rbtn_D.Name = "rbtn_D";
+            this.rbtn_D.Size = new System.Drawing.Size(33, 17);
+            this.rbtn_D.TabIndex = 4;
+            this.rbtn_D.TabStop = true;
+            this.rbtn_D.Text = "D";
+            this.rbtn_D.UseVisualStyleBackColor = true;
+            // 
+            // btn_next
+            // 
+            this.btn_next.Location = new System.Drawing.Point(284, 53);
+            this.btn_next.Name = "btn_next";
+            this.btn_next.Size = new System.Drawing.Size(97, 23);
+            this.btn_next.TabIndex = 5;
+            this.btn_next.Text = "Cấu tiếp theo";
+            this.btn_next.UseVisualStyleBackColor = true;
+            this.btn_next.Click += new System.EventHandler(this.btn_next_Click);
+            // 
+            // btn_back
+            // 
+            this.btn_back.Location = new System.Drawing.Point(294, 103);
+            this.btn_back.Name = "btn_back";
+            this.btn_back.Size = new System.Drawing.Size(75, 23);
+            this.btn_back.TabIndex = 6;
+            this.btn_back.Text = "Câu trước ";
+            this.btn_back.UseVisualStyleBackColor = true;
+            this.btn_back.Click += new System.EventHandler(this.btn_back_Click);
+            // 
+            // btn_send
+            // 
+            this.btn_send.Location = new System.Drawing.Point(294, 149);
+            this.btn_send.Name = "btn_send";
+            this.btn_send.Size = new System.Drawing.Size(75, 23);
+            this.btn_send.TabIndex = 7;
+            this.btn_send.Text = "Nộp";
+            this.btn_send.UseVisualStyleBackColor = true;
+            // 
+            // lb_2
+            // 
+            this.lb_2.AutoSize = true;
+            this.lb_2.Location = new System.Drawing.Point(25, 9);
+            this.lb_2.Name = "lb_2";
+            this.lb_2.Size = new System.Drawing.Size(41, 13);
+            this.lb_2.TabIndex = 8;
+            this.lb_2.Text = "Câu 2 :";
+            // 
+            // lb_3
+            // 
+            this.lb_3.AutoSize = true;
+            this.lb_3.Location = new System.Drawing.Point(25, 9);
+            this.lb_3.Name = "lb_3";
+            this.lb_3.Size = new System.Drawing.Size(41, 13);
+            this.lb_3.TabIndex = 9;
+            this.lb_3.Text = "Câu 3 :";
+            // 
+            // lb_4
+            // 
+            this.lb_4.AutoSize = true;
+            this.lb_4.Location = new System.Drawing.Point(25, 9);
+            this.lb_4.Name = "lb_4";
+            this.lb_4.Size = new System.Drawing.Size(41, 13);
+            this.lb_4.TabIndex = 10;
+            this.lb_4.Text = "Câu 4 :";
+            // 
+            // lb_5
+            // 
+            this.lb_5.AutoSize = true;
+            this.lb_5.Location = new System.Drawing.Point(25, 9);
+            this.lb_5.Name = "lb_5";
+            this.lb_5.Size = new System.Drawing.Size(41, 13);
+            this.lb_5.TabIndex = 11;
+            this.lb_5.Text = "Câu 5 :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 211);
+            this.Controls.Add(this.lb_5);
+            this.Controls.Add(this.lb_4);
+            this.Controls.Add(this.lb_3);
+            this.Controls.Add(this.lb_2);
+            this.Controls.Add(this.btn_send);
+            this.Controls.Add(this.btn_back);
+            this.Controls.Add(this.btn_next);
+            this.Controls.Add(this.rbtn_D);
+            this.Controls.Add(this.rbtn_C);
+            this.Controls.Add(this.rbtn_B);
+            this.Controls.Add(this.rbtn_A);
+            this.Controls.Add(this.lb_1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lb_1;
+        private System.Windows.Forms.RadioButton rbtn_A;
+        private System.Windows.Forms.RadioButton rbtn_B;
+        private System.Windows.Forms.RadioButton rbtn_C;
+        private System.Windows.Forms.RadioButton rbtn_D;
+        private System.Windows.Forms.Button btn_next;
+        private System.Windows.Forms.Button btn_back;
+        private System.Windows.Forms.Button btn_send;
+        private System.Windows.Forms.Label lb_2;
+        private System.Windows.Forms.Label lb_3;
+        private System.Windows.Forms.Label lb_4;
+        private System.Windows.Forms.Label lb_5;
+    }
+}
+
---
+++ TracNghiem_BTlop/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TracNghiem_BTlop
+{
+    public partial class Form1 : Form
+    {
+        private Label[] questionLabels;
+        private int currentQuestionIndex;
+        private RadioButton[] answerOptions;
+        private bool[][] questionStates;
+        private Dictionary<int, int> radioStates = new Dictionary<int, int>();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Khởi tạo mảng các nhãn câu hỏi
+            questionLabels = new Label[]
+    {
+        lb_1,
+        lb_2,
+        lb_3,
+        lb_4,
+        lb_5
+    };
+
+            // Khởi tạo mảng các nút radio
+            answerOptions = new RadioButton[]
+    {
+        rbtn_A,
+        rbtn_B,
+        rbtn_C,
+        rbtn_D
+    };
+            // Khởi tạo mảng trạng thái cho các nút radio
+            questionStates = new bool[questionLabels.Length][];
+            for (int i = 0; i < questionLabels.Length; i++)
+            {
+                questionStates[i] = new bool[answerOptions.Length];
+            }
+
+            // Ẩn tất cả các nhãn câu hỏi khi form được tải lên  
+            HideAllQuestionLabels();
+            questionLabels[0].Visible = true;
+
+            // Khởi tạo chỉ số câu hỏi hiện tại
+            currentQuestionIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadRadioState();
+            SaveRadioState();
+            //questionLabels[currentQuestionIndex].Visible = true;
+        }
+
+        private void HideAllQuestionLabels()
+        {
+            foreach (Label questionLabel in questionLabels)
+            {
+                questionLabel.Visible = false;
+            }
+        }
+
+        private void ClearAllSelections()
+        {
+            foreach (RadioButton radioButton in answerOptions)
+            {
+                radioButton.Checked = false;
+            }
+        }
+
+        private void SaveRadioState()
+        {
+            for (int i = 0; i < answerOptions.Length; i++)
+            {
+                questionStates[currentQuestionIndex][i] = answerOptions[i].Checked;
+            }
+        }
+
+        private void LoadRadioState()
+        {
+            for (int i = 0; i < answerOptions.Length; i++)
+            {
+                answerOptions[i].CheckedChanged -= RadioButton_CheckedChanged;
+                answerOptions[i].Checked = questionStates[currentQuestionIndex][i];
+                answerOptions[i].CheckedChanged += RadioButton_CheckedChanged;
+            }
+        }
+
+        private void RadioButton_CheckedChanged(object sender, EventArgs e)
+        {
+            RadioButton radioButton = sender as RadioButton;
+            if (radioButton != null && radioButton.Checked)
+            {
+                // Lưu trạng thái của nút radio đã được chọn
+                int answerIndex = Array.IndexOf(answerOptions, radioButton);
+                questionStates[currentQuestionIndex][answerIndex] = true;
+            }
+        }
+
+        private void btn_next_Click(object sender, EventArgs e)
+        {   
+            // Ẩn câu hỏi hiện tại
+            questionLabels[currentQuestionIndex].Visible = false;
+
+            // Đặt lại trạng thái của các nút radio
+            ClearAllSelections();
+
+            // Tăng chỉ số câu hỏi hiện tại
+            currentQuestionIndex++;
+
+            // Kiểm tra nếu đã đạt đến câu hỏi cuối cùng,
+            // thì không tăng chỉ số nữa và hiển thị câu hỏi cuối cùng
+            if (currentQuestionIndex >= questionLabels.Length)
+            {
+                currentQuestionIndex = questionLabels.Length - 1;
+            }
+
+            // Hiển thị câu hỏi tiếp theo
+            questionLabels[currentQuestionIndex].Visible = true;
+
+            // Tải lại trạng thái của các nút radio
+            LoadRadioState();
+        }
+
+        private void btn_back_Click(object sender, EventArgs e)
+        {
+            // Lưu trạng thái của các nút radio
+            SaveRadioState();
+
+            // Giảm chỉ số câu hỏi hiện tại
+            currentQuestionIndex--;
+
+            // Kiểm tra nếu đã đạt đến câu hỏi đầu tiên,
+            // thì không giảm chỉ số nữa và hiển thị câu hỏi đầu tiên
+            if (currentQuestionIndex < 0)
+            {
+                currentQuestionIndex = 0;
+            }
+
+            // Ẩn câu hỏi hiện tại
+            questionLabels[currentQuestionIndex + 1].Visible = false;
+
+            // Hiển thị câu hỏi trước đó
+            questionLabels[currentQuestionIndex].Visible = true;
+
+            // Tải lại trạng thái của các nút radio```csharp
+            LoadRadioState();
+        }
+    }
+}
